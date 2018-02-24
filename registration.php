fb65a455818f9ccbb616d3e8739ca7144744b8ea
Paul Hachmang <paul@h-o.nl>
2018-02-24 11:07:27 +0100

    Composer fixes

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ReachDigital_ProophEventStore',
+    __DIR__.'/src'
+);
