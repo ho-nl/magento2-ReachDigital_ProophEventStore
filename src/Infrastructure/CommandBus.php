887a08656aa2402b88453e18d07044637f393720
Maikel Koek <maikel@h-o.nl>
2018-04-06 15:06:20 +0200

    [VM-191] +: Dispatch Magento event before and after command bus dispatch

--- src/Infrastructure/CommandBus.php
+++ src/Infrastructure/CommandBus.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+
 declare(strict_types=1);
 
 namespace ReachDigital\ProophEventStore\Infrastructure;
 
+use Magento\Framework\Event\Manager;
 use Prooph\Common\Event\ActionEventEmitter;
 
 class CommandBus extends \Prooph\ServiceBus\CommandBus
@@ -10,17 +16,38 @@ class CommandBus extends \Prooph\ServiceBus\CommandBus
     /** @var CommandRouter */
     private $commandRouter;
 
+    /**
+     * @var Manager
+     */
+    private $eventManager;
+
     public function __construct(
         ActionEventEmitter $actionEventEmitter = null,
-        CommandRouter $commandRouter
+        CommandRouter $commandRouter,
+        Manager $eventManager
     ) {
         parent::__construct($actionEventEmitter);
         $this->commandRouter = $commandRouter;
+        $this->eventManager = $eventManager;
         $commandRouter->attachToMessageBus($this);
     }
 
     public function commandRouter()
     {
         return $this->commandRouter;
+    }
+
+    /**
+     * Extended to dispatch Magento events
+     *
+     * @param mixed $command
+     */
+    public function dispatch($command): void
+    {
+        $this->eventManager->dispatch('prooph_command_bus_dispatch_before', ['command' => $command]);
+
+        parent::dispatch($command);
+
+        $this->eventManager->dispatch('prooph_command_bus_dispatch_after', ['command' => $command]);
     }
 }
