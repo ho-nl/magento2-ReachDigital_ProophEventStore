c5ee3e9f7e512631cd0fb7df8d9bc6f4ba3b4af2
Paul Hachmang <paul@h-o.nl>
2018-10-09 10:46:15 +0200

    Added UninstallSchema to remove the prooph-event-store completely.

---
+++ src/Setup/UninstallSchema.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+namespace ReachDigital\ProophEventStore\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+
+class UninstallSchema implements UninstallInterface
+{
+
+    /**
+     * Invoked when remove-data flag is set during module uninstall.
+     *
+     * @param SchemaSetupInterface   $setup
+     * @param ModuleContextInterface $context
+     *
+     * @return void
+     */
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if ($setup->getConnection()->isTableExists('event_streams')) {
+            $setup->getConnection()->dropTable('event_streams');
+        }
+        if ($setup->getConnection()->isTableExists('projections')) {
+            $setup->getConnection()->dropTable('projections');
+        }
+        if ($setup->getConnection()->isTableExists('snapshots')) {
+            $setup->getConnection()->dropTable('snapshots');
+        }
+
+        $setup->endSetup();
+    }
+}
