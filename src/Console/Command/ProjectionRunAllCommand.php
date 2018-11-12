bbb83c09c954ca4fc110adfee55941abf054164f
Paul Hachmang <paul@h-o.nl>
2018-11-12 09:38:11 +0100

    Implemented a RunAll command to start all projections, fixed issue where the Run command did not clean up after it'sself.

--- src/Console/Command/ProjectionRunAllCommand.php
+++ src/Console/Command/ProjectionRunAllCommand.php
@@ -8,46 +8,39 @@ declare(strict_types=1);
 namespace ReachDigital\ProophEventStore\Console\Command;
 
 use Magento\Framework\Shell;
-use Prooph\EventStore\Projection\ProjectionManager;
-use Prooph\EventStore\Projection\Projector;
 use ReachDigital\ProophEventStore\Infrastructure\Projection\ProjectionContextPool;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ProjectionRunAllCommand extends \Symfony\Component\Console\Command\Command
 {
+    use FormatsOutput;
+
     public const OPTION_RUN_ONCE = 'run-once';
 
-    /**
-     * @var ProjectionContextPool
-     */
+    /** @var ProjectionContextPool */
     private $projectionContextPool;
 
-    /**
-     * @var ProjectionManager
-     */
-    private $projectionManager;
-
-    /**
-     * @var Shell
-     */
+    /** @var Shell */
     private $shell;
+
+    /** @var \Symfony\Component\Process\PhpExecutableFinder */
+    private $phpExecutableFinder;
 
     public function __construct(
         ProjectionContextPool $projectionContextPool,
-        ProjectionManager $projectionManager,
+        \Magento\Framework\Process\PhpExecutableFinderFactory $phpExecutableFinderFactory,
         Shell $shell,
         ?string $name = null
     ) {
         parent::__construct($name);
         $this->projectionContextPool = $projectionContextPool;
-        $this->projectionManager = $projectionManager;
+        $this->phpExecutableFinder = $phpExecutableFinderFactory->create();
         $this->shell = $shell;
     }
 
-    protected function configure()
+    protected function configure() : void
     {
         parent::configure();
         $this
@@ -58,31 +51,27 @@ class ProjectionRunAllCommand extends \Symfony\Component\Console\Command\Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->formatOutput($output);
+
         $keepRunning = ! $input->getOption(static::OPTION_RUN_ONCE);
         foreach ($this->projectionContextPool->all() as $projectionContext) {
             $output->writeln(
                 sprintf(
-                    '<action>Starting projection <highlight>%s</highlight>. Keep running: <highlight>%s</highlight></action>',
+                    '<action>Starting projection <highlight>%s</highlight> in background. Keep running: <highlight>%s</highlight></action>',
                     $projectionContext->name(),
                     $keepRunning === true ? 'enabled' : 'disabled'
                 )
             );
 
-
-            $this->shell->execute('./bin/magento event-store:projection:run');
-
-
-            $pid = pcntl_fork();
-            if ($pid === -1) {
-                exit("Error forking...\n");
-            }
-
-            if ($pid === 0) {
-                $projector = $projectionContext->projector([Projector::OPTION_PCNTL_DISPATCH => true]);
-                $projection = $projectionContext->projection()->project($projector);
-                $projection->run($keepRunning);
-                exit();
-            }
+            $phpPath = $this->phpExecutableFinder->find() ?: 'php';
+            $runOnce = $input->getOption(static::OPTION_RUN_ONCE) ? '-o' : '';
+            $this->shell->execute(
+                "{$phpPath} %s event-store:projection:run {$runOnce} %s > /dev/null 2>&1 &",
+                [
+                    BP . '/bin/magento',
+                    $projectionContext->name()
+                ]
+            );
         }
     }
 }
